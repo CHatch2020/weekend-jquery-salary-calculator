0ba5dc5017988592f5f1157ea43e0be2f2f5dfc7
Caleb Hatch <hatchcaleb@gmail.com>
2021-11-06 10:55:49 -0500

    Renders employee object to DOM

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 $(document).ready(onReady);
 
+let employees = [
+    {
+        first: 'James',
+        last: 'Stewart',
+        id: 4536,
+        title: 'Team Manager',
+        annualSalary: 654000,
+    }
+];
+
+
 function onReady(){
-    console.log('hello');
+    renderEmployeeData(employees);
+    
+    
     
 };// end onReady
+
+
+function renderEmployeeData(empToRender){
+    $('#tableBody').empty();
+
+    for (const employee of empToRender) {
+        let newTableRow = `
+        <tr>
+            <td>${employee.first}</td>
+            <td>${employee.last}</td>
+            <td>${employee.id}</td>
+            <td>${employee.title}</td>
+            <td>${employee.annualSalary}</td>
+        </tr>
+        `
+
+        $('#tableBody').append(newTableRow);
+    }
+    
+};// end renderEmployeeData
