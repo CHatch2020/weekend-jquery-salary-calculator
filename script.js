fc9352e7acf4c28bc20a5e3c1f01fdbc00ac9f28
Caleb Hatch <hatchcaleb@gmail.com>
2021-11-06 11:15:16 -0500

    renders employees to DOM on click

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 $(document).ready(onReady);
 
-let employees = [
-    {
-        first: 'James',
-        last: 'Stewart',
-        id: 4536,
-        title: 'Team Manager',
-        annualSalary: 654000,
-    }
-];
+let employees = [];
 
 
 function onReady(){
     renderEmployeeData(employees);
-    
+    $('#submitButton').on('click', handleOnClicks)
     
     
 };// end onReady
 
 
 function renderEmployeeData(empToRender){
+    // empty the table body
     $('#tableBody').empty();
 
+    // loop through through the empToRender parameter and create new table row
     for (const employee of empToRender) {
         let newTableRow = `
         <tr>
@@ -32,8 +26,37 @@ function renderEmployeeData(empToRender){
             <td>${employee.annualSalary}</td>
         </tr>
         `
-
+        // append the new row to the table body
         $('#tableBody').append(newTableRow);
     }
     
 };// end renderEmployeeData
+
+
+function handleOnClicks(){
+// grab user values
+let newFirst = $('#firstName').val();
+let newLast = $('#lastName').val();
+let newEmployeeId = $('#employeeId').val();
+let newEmployeeTitle = $('#employeeTitle').val();
+let newAnnualSalary = $('#annualSalary').val();
+
+// assign values to new object
+let newEmployee = {
+    first: newFirst,
+    last: newLast,
+    id: Number(newEmployeeId),
+    title: newEmployeeTitle,
+    annualSalary: Number(newAnnualSalary),
+};
+// push new object to employees array
+employees.push(newEmployee);
+//empty variables
+$('#firstName').val('');
+$('#lastName').val('');
+$('#employeeId').val('');
+$('#employeeTitle').val('');
+$('#annualSalary').val('');
+// call render function
+renderEmployeeData(employees);
+}
